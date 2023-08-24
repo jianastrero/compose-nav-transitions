a285dfe58e66a6e4ef90efb40236f0835a34c55e
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-24 13:42:26 +0800

    Add "compose-nav-transition" to example dependencies
    Added "compose-nav-transition" as a project dependency in the build.gradle.kts file. This library module includes essential build configuration rules and unit tests, enabling separation and isolation of its source code from other modules.

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    implementation(project(":compose-nav-transition"))
+
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
