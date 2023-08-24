fd61a953470327a0d1173e2c4ecf99f6afea644f
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-25 00:14:39 +0800

    Updated MainActivity.kt to include navigation components
    In MainActivity.kt, the simple greeting text was replaced with a navigation graph which includes "Home" and "Detail" composable. This change allows an easier navigation between different screens in the app. The "Home" screen now also includes a clickable image that navigates to "Detail" when clicked.

--- example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
+++ example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
@@ -3,41 +3,86 @@ package dev.jianastrero.compose_nav_transition.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import dev.jianastrero.compose_nav_transition.example.ui.theme.ComposeNavTransitionTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dev.jianastrero.compose_nav_transition.navigation.NavTransitionHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeNavTransitionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            MainNavGraph()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainNavGraph() {
+    val navController = rememberNavController()
+    NavTransitionHost(
+        navController = navController,
+        startDestination = "Home",
+        modifier = Modifier.fillMaxSize()
+    ) {
+        composable("Home") {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = "Home",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.sample),
+                    contentDescription = "Sample Image",
+                    modifier = Modifier.size(100.dp)
+                        .clickable { navController.navigate("Detail") }
+                )
+            }
+        }
+        composable("Detail") {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Text(
+                    text = "Detail",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.sample),
+                    contentDescription = "Sample Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(240.dp)
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeNavTransitionTheme {
-        Greeting("Android")
-    }
+fun MainNavGraphPreview() {
+    MainNavGraph()
 }
