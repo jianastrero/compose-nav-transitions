f82600fdc52995bf0c5e79c9d7e0b5a445b49c6d
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-26 04:22:32 +0800

    Update MainActivity with transitionComposable calls
    Transition components in MainActivity have been updated. The compose.navigation library's composable function has been replaced with NavTransition's transitionComposable for smoother animations. Padding, clickable and sharedElement properties have been extended in home and detail components to support shared Element transition. This improves the user experience by providing a more fluid navigation between the "Home" and "Detail" sections.

--- example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
+++ example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,9 +22,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dev.jianastrero.compose_nav_transition.navigation.NavTransitionHost
+import dev.jianastrero.compose_nav_transition.navigation.navigateTransition
+import dev.jianastrero.compose_nav_transition.navigation.transitionComposable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +44,7 @@ fun MainNavGraph() {
         startDestination = "Home",
         modifier = Modifier.fillMaxSize()
     ) {
-        composable("Home") {
+        transitionComposable("home") {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
@@ -52,21 +54,28 @@ fun MainNavGraph() {
                     text = "Home",
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(32.dp)
+                        .sharedElement("text")
+                        .padding(4.dp)
                 )
                 Image(
                     painter = painterResource(id = R.drawable.sample),
                     contentDescription = "Sample Image",
-                    modifier = Modifier.size(100.dp)
-                        .clickable { navController.navigate("Detail") }
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clickable { navController.navigateTransition("Detail") }
+                        .sharedElement("image")
                 )
             }
         }
-        composable("Detail") {
+        transitionComposable("detail") {
             Column(modifier = Modifier.fillMaxSize()) {
                 Text(
                     text = "Detail",
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold,
+                    modifier = Modifier.sharedElement("text")
                 )
                 Image(
                     painter = painterResource(id = R.drawable.sample),
@@ -75,6 +84,7 @@ fun MainNavGraph() {
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(240.dp)
+                        .sharedElement("image")
                 )
             }
         }
