559d070aa7b8192fa008f9a9ad9a11754e1f8343
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-26 04:20:33 +0800

    Add NavTransitions class for navigation transitions
    A new NavTransitions class has been added in the dev.jianastrero.compose_nav_transition package. This class will manage shared screen elements and offer a cleaner approach to screen transitions. It introduces host offset, last route tracking and a method to add shared elements for a given route. This change allows for smoother transitions between navigation states by holding onto elements from the previous state with a new value `_screenSharedElements` which superseded the need for the sharedElement method from Modifier.kt thus it was removed.

---
+++ compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/NavTransitions.kt
@@ -0,0 +1,27 @@
+package dev.jianastrero.compose_nav_transition
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+
+internal object NavTransitions {
+    var hostOffset = Offset.Zero
+    var lastRoute: String by mutableStateOf("")
+    private var _screenSharedElements: Map<String, Map<String, Rect>> by mutableStateOf(emptyMap())
+
+    val screenSharedElements: Map<String, Map<String, Rect>>
+        get() = _screenSharedElements
+
+    fun addSharedElement(route: String, item: Pair<String, Rect>) {
+        val routeSharedElements = (_screenSharedElements[route] ?: emptyMap()) + item
+        _screenSharedElements = _screenSharedElements + (route to routeSharedElements)
+    }
+
+    fun keysFor(firstRoute: String, secondRoute: String): Collection<String> {
+        val firstKeys = screenSharedElements[firstRoute]?.keys ?: emptySet()
+        val secondKeys = screenSharedElements[secondRoute]?.keys ?: emptySet()
+        return firstKeys.intersect(secondKeys)
+    }
+}
