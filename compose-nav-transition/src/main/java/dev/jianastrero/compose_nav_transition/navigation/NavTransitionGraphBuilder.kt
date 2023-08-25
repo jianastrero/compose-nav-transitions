237cfba28044bdb3ab1dabda93e1aa6f0b383a97
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-25 13:00:25 +0800

    Add transitionComposable function for easier navigation transitions
    Added transitionComposable function to NavTransitionGraphBuilder.kt in compose-nav-transition module. This provides more control over navigation transitions by allowing users to define their own Composable content. It should improve user interface flows with customized transition animations.

--- compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/navigation/NavTransitionGraphBuilder.kt
+++ compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/navigation/NavTransitionGraphBuilder.kt
@@ -1,10 +1,30 @@
 package dev.jianastrero.compose_nav_transition.navigation
 
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavigatorProvider
+import androidx.navigation.compose.composable
+import dev.jianastrero.compose_nav_transition.NavTransition
+import dev.jianastrero.compose_nav_transition.animation.NavTransitionContainer
 
 class NavTransitionGraphBuilder(
     provider: NavigatorProvider,
     startDestination: String,
     route: String?
-) : NavGraphBuilder(provider, startDestination = startDestination, route = route)
+) : NavGraphBuilder(provider, startDestination, route)
+
+fun NavTransitionGraphBuilder.transitionComposable(
+    route: String,
+    content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit
+) {
+    composable(route = route) {
+        NavTransition.clearCurrentElements()
+        NavTransitionContainer(modifier = Modifier.fillMaxSize()) {
+            content(it)
+        }
+    }
+}
