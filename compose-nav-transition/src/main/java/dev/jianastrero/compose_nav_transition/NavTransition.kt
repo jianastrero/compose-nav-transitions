274b2dd57dcf6a0003ed76c6ae55f52f4d97f5b7
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-25 12:30:47 +0800

    Add NavTransition object to manage navigation transitions
    This commit introduces a new file, NavTransition.kt, in the 'compose-nav-transition' package. It defines an internal object, NavTransition, that holds a map of current navigation elements and provides functions to clear, get, or add to this map. This change is essential for handling navigation transition states and providing smooth navigation transitions in the Compose Nav Transition library.

---
+++ compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/NavTransition.kt
@@ -0,0 +1,28 @@
+package dev.jianastrero.compose_nav_transition
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+
+internal object NavTransition {
+    var hostOffset = Offset.Zero
+    private var _currentElements: Map<String, Rect> by mutableStateOf(mapOf())
+
+    val currentElements: Map<String, Rect>
+        get() = _currentElements
+
+    fun clearCurrentElements() {
+        _currentElements = mapOf()
+    }
+
+    fun getCurrentElement(tag: String): Rect? = _currentElements[tag]
+
+    fun addToCurrentElement(tag: String, rect: Rect) {
+        Log.d("JIANDDEBUG", "adding new element: $tag -> $rect")
+        _currentElements = _currentElements + (tag to rect)
+    }
+}
+
