9cee958f83f2137a59cda81260efedf0242b2d37
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-26 04:21:35 +0800

    Refactor NavTransitionGraphBuilder to improve transitions
    Refactored the NavTransitionGraphBuilder class to improve handling of transitions. The AnimatedContentScope import was removed, and a new 'remember' function was added to record the last navigated route. This change aims to enhance smoothness and continuity of navigation throughout the application.

--- compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/navigation/NavTransitionGraphBuilder.kt
+++ compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/navigation/NavTransitionGraphBuilder.kt
@@ -1,14 +1,13 @@
 package dev.jianastrero.compose_nav_transition.navigation
 
-import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavigatorProvider
 import androidx.navigation.compose.composable
-import dev.jianastrero.compose_nav_transition.NavTransition
 import dev.jianastrero.compose_nav_transition.animation.NavTransitionContainer
 
 class NavTransitionGraphBuilder(
@@ -19,12 +18,15 @@ class NavTransitionGraphBuilder(
 
 fun NavTransitionGraphBuilder.transitionComposable(
     route: String,
-    content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit
+    content: @Composable NavTransitionScope.(NavBackStackEntry) -> Unit
 ) {
-    composable(route = route) {
-        NavTransition.clearCurrentElements()
-        NavTransitionContainer(modifier = Modifier.fillMaxSize()) {
-            content(it)
+    composable(route = route) { navBackStackEntry ->
+        val scope = remember(route) {
+            NavTransitionScope(route)
+        }
+
+        scope.NavTransitionContainer(modifier = Modifier.fillMaxSize()) {
+            scope.content(navBackStackEntry)
         }
     }
 }
