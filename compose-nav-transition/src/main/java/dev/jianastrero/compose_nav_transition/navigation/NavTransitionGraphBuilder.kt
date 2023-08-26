289669e36ddf61d24f7f3d51defe80549204e39d
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-26 17:43:05 +0800

    Improve navigation transition state management
    During navigation transitions in `NavTransitionGraphBuilder.kt`, a new mechanism for state management has been introduced with a variable called 'previousRoute'. Now, every time a lifecycle event occurs, specifically `ON_START`, a function will be called `onScopeChanged()` where the previously navigated route will be remembered using `remember(route)`. The purpose of this change is to refine the navigation handling efficiency by storing the previously accessed navigation route.

--- compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/navigation/NavTransitionGraphBuilder.kt
+++ compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/navigation/NavTransitionGraphBuilder.kt
@@ -2,8 +2,12 @@ package dev.jianastrero.compose_nav_transition.navigation
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavigatorProvider
@@ -13,7 +17,8 @@ import dev.jianastrero.compose_nav_transition.animation.NavTransitionContainer
 class NavTransitionGraphBuilder(
     provider: NavigatorProvider,
     startDestination: String,
-    route: String?
+    route: String?,
+    internal val onScopeChanged: (NavTransitionScope) -> Unit
 ) : NavGraphBuilder(provider, startDestination, route)
 
 fun NavTransitionGraphBuilder.transitionComposable(
@@ -21,12 +26,27 @@ fun NavTransitionGraphBuilder.transitionComposable(
     content: @Composable NavTransitionScope.(NavBackStackEntry) -> Unit
 ) {
     composable(route = route) { navBackStackEntry ->
+        val lifecycleOwner = LocalLifecycleOwner.current
         val scope = remember(route) {
             NavTransitionScope(route)
         }
 
         scope.NavTransitionContainer(modifier = Modifier.fillMaxSize()) {
             scope.content(navBackStackEntry)
+        }
+
+        DisposableEffect(lifecycleOwner) {
+            val observer = LifecycleEventObserver { _, event ->
+                if (event == Lifecycle.Event.ON_START) {
+                    onScopeChanged(scope)
+                }
+            }
+
+            lifecycleOwner.lifecycle.addObserver(observer)
+
+            onDispose {
+                lifecycleOwner.lifecycle.removeObserver(observer)
+            }
         }
     }
 }
