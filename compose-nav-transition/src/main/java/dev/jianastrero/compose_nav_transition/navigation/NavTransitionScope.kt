794353188abd3e99b9b129c6befc2f789ef5877f
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-26 17:41:22 +0800

    Add previousRoute variable in NavTransitionScope
    Added a new variable 'previousRoute' initialized with an empty string in 'NavTransitionScope' class in NavTransitions.kt. This variable is expected to hold the previous navigation route improving the navigation state tracking and leading to a more effective navigation handling. The code efficiency is achieved by better navigation state management.

--- compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/navigation/NavTransitionScope.kt
+++ compose-nav-transition/src/main/java/dev/jianastrero/compose_nav_transition/navigation/NavTransitionScope.kt
@@ -1,12 +1,19 @@
 package dev.jianastrero.compose_nav_transition.navigation
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInRoot
 import dev.jianastrero.compose_nav_transition.NavTransitions
 
-class NavTransitionScope(internal val route: String) {
+class NavTransitionScope(
+    internal val route: String
+) {
+    internal var previousRoute: String by mutableStateOf("")
+
     fun Modifier.sharedElement(tag: String): Modifier = onGloballyPositioned {
         val rect = with(it.positionInRoot()) {
             Rect(
