940a7b244a0906b5a224f8453a27c2bff300b431
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-27 22:46:34 +0800

    Use new API for customizing shared elements
    Updated the sharedElement function in MainActivity.kt to utilise the remember function for text and image elements. This refactoring promotes code reusability by creating and remembering shared elements like text and image for each screen, enhancing the performance of screen transitions. It also leverages textModifier for consistent spacing across TextElements and improves animation by inheriting paddings after "sharedElement" function call.

--- example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
+++ example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -23,6 +24,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
+import dev.jianastrero.compose_nav_transition.element.ImageElement
+import dev.jianastrero.compose_nav_transition.element.TextElement
 import dev.jianastrero.compose_nav_transition.navigation.NavTransitionHost
 import dev.jianastrero.compose_nav_transition.navigation.NavTransitionScope
 import dev.jianastrero.compose_nav_transition.navigation.transitionComposable
@@ -58,27 +61,54 @@ private fun NavTransitionScope.HomeScreen(
     navigate: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val text = remember { "Home" }
+    val fontSize = remember { 24.sp }
+    val fontWeight = remember { FontWeight.Bold }
+    val textModifier = remember { Modifier.padding(4.dp) }
+
+    val imagePainter = painterResource(id = R.drawable.sample)
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier
     ) {
         Text(
-            text = "Home",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
+            text = text,
+            fontSize = fontSize,
+            fontWeight = fontWeight,
             modifier = Modifier
                 .padding(32.dp)
-                .sharedElement("text")
-                .padding(4.dp)
+                .sharedElement(
+                    tag = "text",
+                    element = TextElement(
+                        text = text,
+                        fontSize = fontSize,
+                        fontWeight = fontWeight,
+                        modifier = textModifier
+                        /**
+                         * The only modifier you need to add are the ones after the "sharedElement" function call.
+                         */
+                    )
+                )
+                /**
+                 *  The paddings after the "sharedElement" function call are inherited by the animation.
+                 */
+                .then(textModifier)
         )
         Image(
-            painter = painterResource(id = R.drawable.sample),
+            painter = imagePainter,
             contentDescription = "Sample Image",
             modifier = Modifier
                 .size(100.dp)
                 .clickable { navigate("detail") }
-                .sharedElement("image")
+                .sharedElement(
+                    "image",
+                    element = ImageElement(
+                        painter = imagePainter,
+                        contentScale = ContentScale.Crop // You might want to add content scale for better animations.
+                    )
+                )
         )
     }
 }
@@ -87,21 +117,40 @@ private fun NavTransitionScope.HomeScreen(
 private fun NavTransitionScope.DetailScreen(
     modifier: Modifier
 ) {
+    val text = remember { "Detail" }
+    val fontSize = remember { 32.sp }
+    val fontWeight = remember { FontWeight.Bold }
+
+    val imagePainter = painterResource(id = R.drawable.sample)
+
     Column(modifier = modifier) {
         Text(
-            text = "Detail",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.sharedElement("text")
+            text = text,
+            fontSize = fontSize,
+            fontWeight = fontWeight,
+            modifier = Modifier.sharedElement(
+                tag = "text",
+                element = TextElement(
+                    text = text,
+                    fontSize = fontSize,
+                    fontWeight = fontWeight
+                )
+            )
         )
         Image(
-            painter = painterResource(id = R.drawable.sample),
+            painter = imagePainter,
             contentDescription = "Sample Image",
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(240.dp)
-                .sharedElement("image")
+                .sharedElement(
+                    "image",
+                    element = ImageElement(
+                        painter = imagePainter,
+                        contentScale = ContentScale.Crop
+                    )
+                )
         )
     }
 }
