7bf4516f7e2b2ba191b8a1092b867cec781a0a38
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-27 22:58:58 +0800

    Add Constants.kt file with DUMMY_TEXT constant
    A new Constants.kt file is added to define a constant, DUMMY_TEXT, which contains a dummy lorem ipsum text. This constant is useful for multiple usages throughout the application, mainly for testing purposes. Adding this constant promotes DRY (Don't Repeat Yourself) principle, enhancing code readability and maintainability.

---
+++ example/src/main/java/dev/jianastrero/compose_nav_transition/example/Constants.kt
@@ -0,0 +1,14 @@
+package dev.jianastrero.compose_nav_transition.example
+
+object Constants {
+
+    val DUMMY_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam enim augue, imperdiet a odio " +
+            "et, finibus sodales libero. In purus nulla, viverra a ipsum vel, consequat convallis mauris. Donec " +
+            "varius lacus nisl, at convallis risus luctus porta. Proin consectetur rhoncus lacinia. Donec at neque " +
+            "pretium, ullamcorper metus a, viverra elit. Praesent a convallis tortor. Proin non lorem mauris. Donec " +
+            "posuere felis ex, sit amet suscipit quam pharetra id. In blandit aliquet erat sed scelerisque. Donec " +
+            "feugiat eros sit amet porttitor tempor. Integer et purus a lacus euismod dignissim et vitae enim. Nunc " +
+            "nec ligula mollis, euismod dui sit amet, pharetra purus. Integer vestibulum consequat mi, ac interdum " +
+            "dui maximus in. Mauris ut efficitur ipsum."
+
+}
