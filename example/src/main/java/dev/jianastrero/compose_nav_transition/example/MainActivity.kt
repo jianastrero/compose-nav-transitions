e37137966007bf1f7823c2902a949eadbb03e72c
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-27 22:59:28 +0800

    Add Description text to MainActivity.kt
    In order to enhance readability, Ellipsis text overflow option is added to text elements in MainActivity.kt. This change ensures that if the text is too long to fit on the current display, it's truncated and followed by an ellipsis ('...'), providing an improved user experience in situations where text length exceeds the display capacity.

--- example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
+++ example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -110,6 +111,21 @@ private fun NavTransitionScope.HomeScreen(
                     )
                 )
         )
+        Text(
+            text = Constants.DUMMY_TEXT,
+            maxLines = 3,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .sharedElement(
+                    tag = "description",
+                    element = TextElement(
+                        text = Constants.DUMMY_TEXT,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.padding(horizontal = 32.dp)
+                    )
+                )
+                .padding(horizontal = 32.dp)
+        )
     }
 }
 
@@ -151,6 +167,19 @@ private fun NavTransitionScope.DetailScreen(
                         contentScale = ContentScale.Crop
                     )
                 )
+        )
+        Text(
+            text = Constants.DUMMY_TEXT,
+            modifier = Modifier
+                .sharedElement(
+                    tag = "description",
+                    element = TextElement(
+                        text = Constants.DUMMY_TEXT,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.padding(12.dp)
+                    )
+                )
+                .padding(12.dp)
         )
     }
 }
