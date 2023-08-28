3b1a54df98d62f94562f9707f5007a0c6e853f8f
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-28 10:08:02 +0800

    Add MainScreen with navigation
    Added a new Composable function MainScreen to the screens directory, which includes various UI elements such as a scaffold, grid layout, and navigation elements. This screen has been integrated with the MainNavGraph to provide smooth navigation within the app. Changes have also been made to the MainActivity to accommodate the new MainNavGraph, improving the overall app flow.

---
+++ example/src/main/java/dev/jianastrero/compose_nav_transition/example/screens/MainScreen.kt
@@ -0,0 +1,171 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Jian James Astrero
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package dev.jianastrero.compose_nav_transition.example.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.jianastrero.compose_nav_transition.example.R
+import dev.jianastrero.compose_nav_transition.navigation.NavTransitionScope
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavTransitionScope.MainScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            Header(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+            )
+        },
+        modifier = modifier
+    ) { paddingValues ->
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .padding(12.dp)
+        ) {
+            items(12) {
+                Item(
+                    text = "Spotify Sample",
+                    image = painterResource(id = R.drawable.sample),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun NavTransitionScope.Header(modifier: Modifier = Modifier) {
+    Surface(
+        shadowElevation = 4.dp,
+        modifier = modifier
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "Compose Nav Transitions",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .fillMaxWidth()
+            )
+            Image(
+                imageVector = Icons.Outlined.Notifications,
+                contentDescription = "Notifications",
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .sharedElement("notifications icon")
+                    .clip(CircleShape)
+                    .size(56.dp)
+                    .padding(16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun NavTransitionScope.Item(
+    text: String,
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = text,
+            modifier = Modifier
+                .fillMaxSize()
+                .sharedElement("image")
+        )
+        Spacer(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush  = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black.copy(alpha = 0.75f)
+                        )
+                    )
+                )
+        )
+        Text(
+            text = text,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.labelLarge,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(12.dp)
+                .sharedElement("title")
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun MainScreenPreview() {
+    NavTransitionScope.Preview.MainScreen()
+}
