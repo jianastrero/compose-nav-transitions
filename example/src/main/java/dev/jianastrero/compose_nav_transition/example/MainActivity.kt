2c66e32525ae9279410f834004b7d82760f2a663
Jian James Astrero <jianjamesastrero@gmail.com>
2023-09-17 18:55:36 +0800

    Refactor screen navigation with animations and enum type
    Screen navigation refactored for a smoother user experience. An enum class `ScreenState` was added to manage screen states and to replace the previous mutable integer state. AnimatedVisibility Composable added in MainActivity along with fadeIn and fadeOut animations for screen transitions. Making the application transition between screens more visually pleasing with a fadeIn and fadeOut effect over a duration of 600 milliseconds.

--- example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
+++ example/src/main/java/dev/jianastrero/compose_nav_transition/example/MainActivity.kt
@@ -3,28 +3,43 @@ package dev.jianastrero.compose_nav_transition.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import dev.jianastrero.compose_nav_transition.example.screens.FirstScreen
 import dev.jianastrero.compose_nav_transition.example.screens.SecondScreen
+import dev.jianastrero.compose_nav_transition.example.screens.ScreenState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var screen by rememberSaveable { mutableIntStateOf(1) }
+            var screen: ScreenState by rememberSaveable { mutableStateOf(ScreenState.FirstScreen) }
 
-            when (screen) {
-                1 -> FirstScreen(
-                    onGotoSecondScreen = { screen = 2 },
+            AnimatedVisibility(
+                visible = screen == ScreenState.FirstScreen,
+                enter = fadeIn(tween(600)),
+                exit = fadeOut(tween(600))
+            ) {
+                FirstScreen(
+                    onGotoSecondScreen = { screen = ScreenState.SecondScreen },
                     modifier = Modifier.fillMaxSize()
                 )
-                2 -> SecondScreen(
-                    onGotoFirstScreen = { screen = 1 },
+            }
+            AnimatedVisibility(
+                visible = screen == ScreenState.SecondScreen,
+                enter = fadeIn(tween(600)),
+                exit = fadeOut(tween(600))
+            ) {
+                SecondScreen(
+                    onGotoFirstScreen = { screen = ScreenState.FirstScreen },
                     modifier = Modifier.fillMaxSize()
                 )
             }
